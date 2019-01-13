0e5671f47276fcf9719b7007c9e1cf220e264a07
Rabbia Tul-Zohra <rabbia.tul-zohra@bbc.co.uk>
2019-01-13 19:46:56 +0000

    stores comments in db

--- src/comment-form.js
+++ src/comment-form.js
@@ -1,30 +1,55 @@
 import React, { Component } from "react";
+import Amplify, { API } from "aws-amplify";
+import aws_exports from "./aws-exports";
+
+Amplify.configure(aws_exports);
+let apiName = "CommentsCRUD";
+let path = "/Comments";
+const uuidv1 = require("uuid/v1");
 
 class CommentForm extends Component {
   constructor(props) {
     super(props);
     this.handleChange = this.handleChange.bind(this);
     this.addComment = this.addComment.bind(this);
+    this.state = {
+      comments: [
+        {
+          author: "",
+          text: ""
+        }
+      ]
+    };
   }
-  state = {
-    comments: [
-      {
-        author: "",
-        text: ""
-      }
-    ]
-  };
+
+  componentDidMount() {
+    API.get(apiName, path).then(response => {
+      console.log(response);
+    });
+  }
 
   handleChange(event) {
-    this.setState({ text: event.target.value });
+    this.setState({ author: "Rabbia", text: event.target.value });
   }
 
   addComment(event) {
     event.preventDefault();
-    const newComment = {
-      author: "Rabbia",
-      text: this.state.text
+
+    let newComment = {
+      body: {
+        ID: uuidv1(),
+        author: this.state.author,
+        text: this.state.text
+      }
     };
+    API.post(apiName, path, newComment)
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error.response);
+      });
+
     const newComments = this.state.comments.concat(newComment);
     this.setState({ comments: newComments, text: "" });
   }
@@ -46,7 +71,7 @@ class CommentForm extends Component {
         {this.state.comments.map(comment => (
           <div>
             <h4>{comment.author}</h4>
-            <p>{comment.text}</p>
+            <p key={comment.text}>{comment.text}</p>
           </div>
         ))}
       </div>
