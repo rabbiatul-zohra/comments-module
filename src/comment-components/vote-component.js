3af0518fb753bf196c26fbe297678609dd3c4cb9
Rabbia Tul-Zohra <rabbia.tul-zohra@bbc.co.uk>
2019-02-02 23:35:27 +0000

    removes submit button for now

--- src/comment-components/vote-component.js
+++ src/comment-components/vote-component.js
@@ -5,7 +5,6 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const Vote = () => {
   return (
     <div>
-      <input type="submit" className="sp-comment-arrow" />
       <FontAwesomeIcon icon="arrow-up" />
       <FontAwesomeIcon icon="arrow-down" />
     </div>
