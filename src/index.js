9e46d5ec00006b4fddb5be81d7bec5d36658e8e7
Rabbia Tul-Zohra <rabbia.tul-zohra@bbc.co.uk>
2019-02-02 23:34:27 +0000

    adds access to fontawesome icons

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faArrowUp, faArrowDown } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faArrowUp, faArrowDown);
 
 ReactDOM.render(<App />, document.getElementById("root"));
 serviceWorker.unregister();
