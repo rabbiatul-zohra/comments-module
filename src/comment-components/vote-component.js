86dafd49c36aa6008b145d72248077202939d6a9
Rabbia Tul-Zohra <rabbia.tul-zohra@bbc.co.uk>
2019-02-03 16:19:56 +0000

    adds comments likes/dislike counter

--- src/comment-components/vote-component.js
+++ src/comment-components/vote-component.js
@@ -1,14 +1,37 @@
-import React from "react";
-import "../sass/_main.scss";
+import React, { Component } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const Vote = () => {
-  return (
-    <div>
-      <FontAwesomeIcon icon="arrow-up" />
-      <FontAwesomeIcon icon="arrow-down" />
-    </div>
-  );
-};
+class Vote extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      countLikes: 0,
+      countDislikes: 0
+    };
+  }
+
+  countLikes = () => {
+    this.setState({
+      countLikes: this.state.countLikes + 1
+    });
+  };
+  countDislikes = () => {
+    this.setState({ countDislikes: this.state.countDislikes + 1 });
+  };
+  render() {
+    return (
+      <div>
+        <span className={"counter"}>{this.state.countLikes}</span>
+        <button className={"sp-comment-btn"} onClick={this.countLikes}>
+          <FontAwesomeIcon icon="arrow-up" />
+        </button>
+        <button className={"sp-comment-btn"} onClick={this.countDislikes}>
+          <FontAwesomeIcon icon="arrow-down" />
+        </button>
+        <span className={"counter"}>{this.state.countDislikes}</span>
+      </div>
+    );
+  }
+}
 
 export default Vote;
