304eb5020ddbb61cfce4abb3f0f92115a3b46255
Rabbia Tul-Zohra <rabbia.tul-zohra@bbc.co.uk>
2019-02-03 16:18:39 +0000

    moved _main.scss into index

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faArrowUp, faArrowDown } from "@fortawesome/free-solid-svg-icons";
-
+import "./sass/_main.scss";
 library.add(faArrowUp, faArrowDown);
 
 ReactDOM.render(<App />, document.getElementById("root"));
