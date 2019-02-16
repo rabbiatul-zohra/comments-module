e18940e24ac8687d78aeeaeb9dff7be37d6063f4
Rabbia Tul-Zohra <rabbia.tul-zohra@bbc.co.uk>
2019-02-16 02:05:34 +0000

    adds author name on comment list

--- src/comment-components/comment.js
+++ src/comment-components/comment.js
@@ -4,14 +4,18 @@ import Vote from "./vote-component";
 const Timestamp = require("react-timestamp");
 const Comment = props => {
   return (
-    <div>
-      <li>{props.comment.text}</li>
+    <div className="comment">
+      <li className="comment-text">
+        <strong>{props.comment.author}</strong>
+      </li>
       <Timestamp
         time={props.comment.time}
         format="ago"
         includeDay
         autoUpdate={300}
+        className="timestamp"
       />
+      <li className="comment-text">{props.comment.text}</li>
       <Vote />
     </div>
   );
